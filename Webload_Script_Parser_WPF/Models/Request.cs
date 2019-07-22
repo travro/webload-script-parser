99335cca1e35c9fed4b26b62f2dd5e4f8a65a645
TravisWork <travis.melvin@sumtotalsystems.com>
2019-07-22 16:40:22 -0400

    UPDATE Request class
    Added extra constructors, class adopted parsing of correlations

--- Webload_Script_Parser_WPF/Models/Request.cs
+++ Webload_Script_Parser_WPF/Models/Request.cs
@@ -1,13 +1,34 @@
-﻿namespace Webload_Script_Parser_WPF.Models
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Webload_Script_Parser_WPF.Models
 {
     public class Request
     {
-        public Request.RequestVerb Verb { get; set; }
-        public string Parameters { get; set; }
+        private List<Correlation> _correlations;
+        public Request.RequestVerb Verb { get; }
+        public string Parameters { get; }
+        public Correlation[] Correlations => _correlations.ToArray();
 
         public Request(RequestVerb verb, string parameters)
         {
             Verb = verb; Parameters = parameters;
+            _correlations = new List<Correlation>();
+        }
+        public Request(XElement element)
+        {
+            Verb = ParseRequestVerb(element);
+            Parameters = ParseRequestParams(element);
+            _correlations = new List<Correlation>();
+        }
+        public Request(XElement httpHeaderElement, XElement nodeScriptElement)
+        {
+            Verb = ParseRequestVerb(httpHeaderElement);
+            Parameters = ParseRequestParams(httpHeaderElement);
+            if (nodeScriptElement != null && nodeScriptElement.Value.Contains("setCorrelation") && Verb == RequestVerb.GET)
+            {
+                _correlations = CorrelationFactory.GetCorrelations(nodeScriptElement);
+            }
         }
         public enum RequestVerb
         {
@@ -16,6 +37,26 @@
             PUT = 2,
             DELETE = 3,
             CONNECT = 4
+        }
+        private Request.RequestVerb ParseRequestVerb(XElement element)
+        {
+            string val = element.Attribute("Text").Value;
+            if (val.StartsWith(" CONNECT")) return Request.RequestVerb.CONNECT;
+            if (val.StartsWith(" DELETE")) return Request.RequestVerb.DELETE;
+            if (val.StartsWith(" POST")) return Request.RequestVerb.POST;
+            if (val.StartsWith(" PUT")) return Request.RequestVerb.PUT;
+            if (val.StartsWith(" GET")) return Request.RequestVerb.GET;
+            return Request.RequestVerb.GET;
+        }
+        private string ParseRequestParams(XElement element)
+        {
+            string val = element.Attribute("Text").Value;
+            string sumTotalSite = "sumtotaldevelopment.net/";
+            string domain = (val.Contains(sumTotalSite)) ? sumTotalSite : "https://";
+            int domainIndex = val.IndexOf(domain) + domain.Length;
+            int paramIndex = val.IndexOf(' ', domainIndex);
+
+            return val.Substring(domainIndex/*, paramIndex - domainIndex*/);
         }
     }
 }
