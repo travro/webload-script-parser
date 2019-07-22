c15af42494c139c07ca97cf2342dc707aa7fea7f
TravisWork <travis.melvin@sumtotalsystems.com>
2019-07-22 16:42:29 -0400

    UPDATE TransactionBlockParser class
    refactored, offloaded methods to model classes upon adding correlation classes

--- Webload_Script_Parser_WPF/TransactionBlockParser.cs
+++ Webload_Script_Parser_WPF/TransactionBlockParser.cs
@@ -3,77 +3,38 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Linq;
 using Webload_Script_Parser_WPF.Models;
+using System;
 
 namespace Webload_Script_Parser_WPF
 {
     public class TransactionBlockParser
     {
-        private static string ParseTransactionName(XElement element)
-        {
-            string itemName = element.Element("Properties").Element("PropertyPage").Element("ItemName").Value;
-            string beginTrans = "BeginTransaction::";
-            if (itemName.Contains(beginTrans))
-            {
-                return itemName.Substring(itemName.IndexOf(beginTrans) + beginTrans.Length);
-            }
-            else return "Transaction";
-        }
-        private static Request.RequestVerb ParseRequestVerb(XElement element)
-        {
-            string val = element.Attribute("Text").Value;
-            if (val.StartsWith(" CONNECT")) return Request.RequestVerb.CONNECT;
-            if (val.StartsWith(" DELETE")) return Request.RequestVerb.DELETE;
-            if (val.StartsWith(" POST")) return Request.RequestVerb.POST;
-            if (val.StartsWith(" PUT")) return Request.RequestVerb.PUT;
-            if (val.StartsWith(" GET")) return Request.RequestVerb.GET;
-            return Request.RequestVerb.GET;
-        }
-        private static string ParseRequestParams(XElement element)
-        {
-            string val = element.Attribute("Text").Value;
-            string sumTotalSite = "sumtotaldevelopment.net/";
-            string domain = (val.Contains(sumTotalSite)) ? sumTotalSite : "https://";
-            int domainIndex = val.IndexOf(domain) + domain.Length;
-            int paramIndex = val.IndexOf(' ', domainIndex);
-
-            return val.Substring(domainIndex/*, paramIndex - domainIndex*/);
-        }
-
         //Given a filepath and repo, will populate repo with Transaction objects
         public static void Parse(string path, TransactionRepository repo)
         {
-            FileStream _fStream = new FileStream(path, FileMode.Open);
-            XmlReader _xReader = XmlReader.Create(_fStream);
-            XDocument _XDoc = XDocument.Load(_xReader);
-
-            //Get the JavaScriptObject XElements that are BeginTransaction blocks
-            var jScriptElements = from transTable in _XDoc.Descendants("JavaScriptObject")
-                                  where transTable
-                                  .Element("Properties")
-                                  .Element("PropertyPage")
-                                  .Element("ItemName")
-                                  .Value.Contains("BeginTransaction::")
-                                  select transTable;
-
-            //Add each XElement to the repo as a Transaction object
-            foreach (var jSE in jScriptElements)
+            using (FileStream _fStream = new FileStream(path, FileMode.Open))
             {
-                Transaction trans = new Transaction(ParseTransactionName(jSE));
-
-                //Get the PropertyPage descendant elements that are of type HTTPHeader, which contain the request URLs
-                var reqElements = jSE.Descendants("JavaScriptObject")
-                    .Descendants()
-                    .Where(desc => desc.Name == "PropertyPage" && desc.Attribute("Name").Value == "HTTPHeaders")
-                    .Elements();
-
-                foreach (var req in reqElements)
+                using (XmlReader _xReader = XmlReader.Create(_fStream))
                 {
-                    trans.AddRequest(new Request(ParseRequestVerb(req), ParseRequestParams(req)));
+                    XDocument _XDoc = XDocument.Load(_xReader);
+
+                    //Filter out Webload Sleeptime, JSObjects, and End Trasactions
+                    var jsParentBlockElements = _XDoc
+                        .Descendants("Agenda")
+                        .Elements("JavaScriptObject")
+                        .Where(element => element.Element("Properties")
+                        .Element("PropertyPage")
+                        .Element("ItemName")
+                        .Value
+                        .Contains("BeginTransaction::"));
+
+                    //Add each XElement to the repo as a Transaction object
+                    foreach (var jsPBE in jsParentBlockElements)
+                    {
+                        repo.AddTransaction(new Transaction(jsPBE));
+                    }
                 }
-                repo.AddTransaction(trans);
             }
-            _xReader.Dispose();
-            _fStream.Dispose();
         }
     }
 }
