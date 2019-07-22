de622cf3c4fe85f0f80124c00862a7773d6a7aed
TravisWork <travis.melvin@sumtotalsystems.com>
2019-07-22 16:39:17 -0400

    ADD Classes
    added Correlation and Correlation Factory class

---
+++ Webload_Script_Parser_WPF/Models/Correlation.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+
+namespace Webload_Script_Parser_WPF.Models
+{
+    public class Correlation
+    {
+        public string Name { get; set; }
+        public string OriginalValue { get; set; }
+
+        public Correlation( string name, string originalValue)
+        {
+            Name = name; OriginalValue = originalValue;
+        }
+    }
+}
---
+++ Webload_Script_Parser_WPF/Models/CorrelationFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Webload_Script_Parser_WPF.Models
+{
+    public class CorrelationFactory
+    {
+        public static List<Correlation> GetCorrelations(XElement element)
+        {
+            StringBuilder builder = new StringBuilder(element.Value);
+
+
+
+
+
+
+            return new List<Correlation>();
+        }
+    }
+}
