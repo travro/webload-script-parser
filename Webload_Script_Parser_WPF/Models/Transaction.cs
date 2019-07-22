81977922d736c0a60c4b6c2e079ec51bbdeb9634
TravisWork <travis.melvin@sumtotalsystems.com>
2019-07-22 16:40:59 -0400

    UPDATE Transaction Class
    added constructors, class adopted parsing methods from repository

--- Webload_Script_Parser_WPF/Models/Transaction.cs
+++ Webload_Script_Parser_WPF/Models/Transaction.cs
@@ -1,28 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
+
 
 namespace Webload_Script_Parser_WPF.Models
 {
     public class Transaction
     {
-        public List<Request> Requests { get; }
+        private List<Request> _requests;
+        public Request[] Requests => _requests.ToArray();
         public string Name { get; set; }
-
         public Transaction(string name)
         {
             Name = name;
-            Requests = new List<Request>();
+            _requests = new List<Request>();
         }
-        public void AddRequest(Request r)
+        public Transaction(XElement element)
         {
-            Requests.Add(r);
+            Name = ParseTransactionName(element);
+            _requests = new List<Request>();
+
+            var jsChildBlockElements = element.Elements("JavaScriptObject");
+
+            foreach (var jsCBE in jsChildBlockElements)
+            {
+                var httpHeaderElements = jsCBE.Descendants("PropertyPage")
+                    .Where(desc => desc.Attribute("Name").Value == "HTTPHeaders")
+                    .Elements();
+
+                XElement nodeScriptElement = jsCBE.Descendants("PropertyPage")
+                    .Where(desc => desc.Attribute("Name").Value == "JavaScript")
+                    .First()
+                    .Element("NodeScript");
+
+                foreach (var httpHeaderElement in httpHeaderElements)
+                {
+                    _requests.Add(new Request(httpHeaderElement, nodeScriptElement));
+                }
+            }
         }
         public bool Equals(Transaction t)
         {
             return this.Name == t.Name;
+        }
+        private string ParseTransactionName(XElement element)
+        {
+            string itemName = element
+                .Element("Properties")
+                .Element("PropertyPage")
+                .Element("ItemName").Value;
+            string beginTrans = "BeginTransaction::";
+
+            if (itemName.Contains(beginTrans))
+            {
+                return itemName.Substring(itemName.IndexOf(beginTrans) + beginTrans.Length);
+            }
+            else return "Transaction";
         }
     }
 }
