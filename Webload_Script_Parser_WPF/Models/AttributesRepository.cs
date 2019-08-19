741bd756cf0eef22be2a3368a147d8fe1d319222
TravisWork <travis.melvin@sumtotalsystems.com>
2019-08-19 09:52:43 -0400

    ADD AttributesRepostiroy class

---
+++ Webload_Script_Parser_WPF/Models/AttributesRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Webload_Script_Parser_WPF.Models
+{
+    public sealed class AttributesRepository
+    {
+        private static AttributesRepository repo = null;
+        private List<string> _testNames;
+        private List<string> _testBuilds;
+        public string[] TestNames => _testNames.ToArray();
+        public string[] TestBuilds => _testBuilds.ToArray();
+        //private  List<object> _scenarioNames;
+        //private  List<object> _scenarioDates;
+        public static AttributesRepository Repository
+        {
+            get
+            {
+                if (repo == null)
+                {
+                    //lazy loading here
+                    repo = new AttributesRepository();
+                }
+                return repo;
+            }
+        }
+        private AttributesRepository()
+        {
+            _testNames = new List<string>();
+            _testBuilds = new List<string>();
+
+            using (var cnn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["WLScriptsDB"].ConnectionString))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT DISTINCT [NAME] FROM dbo.[TESTS] ORDER BY [NAME]", cnn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            _testNames.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                cnn.Close();
+                cnn.Dispose();
+            }
+
+            using (var cnn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["WLScriptsDB"].ConnectionString))
+            {
+                cnn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT DISTINCT [BUILD] FROM dbo.[TESTS] ORDER BY [BUILD]", cnn))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            _testBuilds.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                cnn.Close();
+                cnn.Dispose();
+            }
+        }
+    }
+}
+
