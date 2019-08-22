109cc2a0e8c922ca05dd451605fe2f82307f5fbe
TravisWork <travis.melvin@sumtotalsystems.com>
2019-08-22 10:08:55 -0400

    ADD DAL

---
+++ Webload_Script_Parser_WPF/DAL/SqlAPI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webload_Script_Parser_WPF.DAL
+{
+    public class SqlAPI
+    {
+    }
+}
