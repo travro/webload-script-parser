36fedd0fd598c550e50fab2a9516b95893e0c900
TravisWork <travis.melvin@sumtotalsystems.com>
2019-08-26 11:33:50 -0400

    ADD TransactionPage

---
+++ Webload_Script_Parser_WPF/Pages/TransactionsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WLScriptParser.Models;
+using WLScriptParser.Controls;
+
+namespace WLScriptParser.Pages
+{
+    /// <summary>
+    /// Interaction logic for TransactionsPage.xaml
+    /// </summary>
+    public partial class TransactionsPage : Page
+    {
+        public TransactionsPage()
+        {
+            InitializeComponent();
+        }
+        public TransactionsPage(
+            TransactionRepository repoLeft, 
+            TransactionRepository repoRight,
+            string fileNameLeft,
+            string fileNameRight)
+        {
+            InitializeComponent();
+            Frame_Left.Content = new ScriptTransactionsControl(repoLeft, fileNameLeft);
+            Frame_Right.Content = new ScriptTransactionsControl(repoRight, fileNameRight);
+        }
+    }
+}
