31082566537a8b164cbb808fa1f488b1e8c13c77
TravisWork <travis.melvin@sumtotalsystems.com>
2019-09-18 16:39:43 -0400

    UPDATE RequestParamParsers
    comparisons more effective as more session and tenant data is filtered out

--- WLScriptParser/Parsers/RequestParamParser.cs
+++ WLScriptParser/Parsers/RequestParamParser.cs
@@ -10,7 +10,7 @@ namespace WLScriptParser.Parsers
     public static class RequestParamParser
     {
         public static Request.RequestVerb ParseRequestVerb(string line)
-        {            
+        {
             if (line.StartsWith(" CONNECT")) return Request.RequestVerb.CONNECT;
             if (line.StartsWith(" DELETE")) return Request.RequestVerb.DELETE;
             if (line.StartsWith(" POST")) return Request.RequestVerb.POST;
@@ -21,13 +21,57 @@ namespace WLScriptParser.Parsers
 
         public static string ParseRequestParamters(string line)
         {
-            //string val = element.Attribute("Text").Value;
+
+            //initially filter out domain and session data
             string sumTotalSite = "sumtotaldevelopment.net";
             string domain = (line.Contains(sumTotalSite)) ? sumTotalSite : "https://";
             int domainLastIndex = line.IndexOf(domain) + domain.Length;
-            //int paramIndex = line.IndexOf(' ', domainLastIndex);
             int paramIndex = line.IndexOfAny(new[] { '?', '%', ' ' }, domainLastIndex);
-            return line.Substring(domainLastIndex, paramIndex - domainLastIndex);
+
+            string filteredUrl = line.Substring(domainLastIndex, paramIndex - domainLastIndex);
+
+            filteredUrl = filteredUrl.ReplaceInBounds("PR##_LM_Test", "/learning/DataStore/", "/Learning/");
+            filteredUrl = filteredUrl.ReplaceInBounds("[TimeStamp]", "blank___", ".");
+            filteredUrl = filteredUrl.ReplaceInBounds("[UserId]", "gamification/summaries/");
+            filteredUrl = filteredUrl.ReplaceInBounds("[BrokerSessionId]", "api/sumtSocial/communities/");
+            filteredUrl = filteredUrl.ReplaceInBounds("[BrokerSessionId]", "api/sumtSocial/communities/", "/");
+            filteredUrl = filteredUrl.ReplaceInBounds("[BrokerSessionId]", "api/social/discuss");
+            //filteredUrl = filteredUrl.ReplaceInBounds("[BrokerSessionID]", "api/sumtSocial/communities/", "ownerCount");
+
+            return filteredUrl;
+        }
+    }
+    //Custom extension methods for checking for different boundaries and replacing internal values if checks are truthy
+    internal static class StringExtension
+    {
+        public static string ReplaceInBounds(this string url, string replacementVal, string leftBound, string rightBound = null)
+        {
+            if (!url.Contains(leftBound)) return url;
+            if (rightBound != null && !url.Substring(url.IndexOf(leftBound) + leftBound.Length).Contains(rightBound)) return url;
+
+
+            int leftBoundIndex = url.IndexOf(leftBound) + leftBound.Length;
+
+            if (leftBoundIndex >= url.Length - 1) return url;
+
+            int rightBoundIndex;
+            string originalValue;
+
+            if (rightBound != null)
+            {
+                rightBoundIndex = url.IndexOf(rightBound, leftBoundIndex);
+                originalValue = url.Substring(leftBoundIndex, rightBoundIndex - leftBoundIndex);
+            }
+            else
+            {
+                originalValue = url.Substring(leftBoundIndex);
+            }
+
+            var strBuilder = new StringBuilder(url);
+            strBuilder.Replace(originalValue, replacementVal);
+            url = strBuilder.ToString();
+            return url;
         }
     }
 }
+
