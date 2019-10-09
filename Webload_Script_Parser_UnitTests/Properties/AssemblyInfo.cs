17854d7a815ec1b6369add974aa5ae18b11adda8
tsmelvin <travis.melvin@sumtotalsystems.com>
2019-10-09 15:47:10 -0400

    UPDATE AssemblyInfo.cs
    updated to 1.1

--- Webload_Script_Parser_UnitTests/Properties/AssemblyInfo.cs
+++ Webload_Script_Parser_UnitTests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("401d1fbc-e4b0-42cb-b28a-a81b2987cdee")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
