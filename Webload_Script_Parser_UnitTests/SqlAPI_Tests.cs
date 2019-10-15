3b1501e0ab1bc9ca8942227ea7643d6c05bc3bc3
tsmelvin <travis.melvin@sumtotalsystems.com>
2019-10-15 13:57:49 -0400

    UPDATE WSP_UnitTests
    Added App.config and Test for SqlApi

---
+++ Webload_Script_Parser_UnitTests/SqlAPI_Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WLScriptParser.DAL;
+
+namespace Webload_Script_Parser_UnitTests
+{
+    [TestClass]
+    public class SqlAPI_Tests
+    {
+        [TestMethod]
+        public void SqlAPI_TestCollections_IsNotNull()
+        {
+            Assert.IsNotNull(SqlAPI.GetTestCollections(WLScriptParser.Models.ScriptAttribute.TestNames));
+            Assert.IsNotNull(SqlAPI.GetTestCollections(WLScriptParser.Models.ScriptAttribute.BuildNames));
+        }
+
+        [TestMethod]
+        public void SqlAPI_TestID_ReturnsCorrectValue()
+        {
+            Assert.AreEqual(1, SqlAPI.GetTestId("LM", "19.3.0.0-211"));
+        }
+
+        //[TestMethod]
+        //public void SqlAPI_PushTest_ReturnsCorrectScopeId()
+        //{
+        //    Assert.AreEqual(11, SqlAPI.PushNewTest("LM", "19.3.0.0-120"));
+        //}
+
+        //[TestMethod]
+        //public void SqlAPI_PushScript_ReturnsCorrectScopeId()
+        //{
+        //    Assert.AreEqual(35, SqlAPI.PushNewScript("AdminGenTask", DateTime.Today.Date, 4));
+        //}
+    }
+}
